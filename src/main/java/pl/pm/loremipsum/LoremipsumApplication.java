aabc8bb2482aba96b73723ea1088058dab80b3ff
Pawel Machulski <pawel.machulski94@gmail.com>
2017-09-01 13:40:39 +0200

    first commit

---
+++ src/main/java/pl/pm/loremipsum/LoremipsumApplication.java
@@ -0,0 +1,12 @@
+package pl.pm.loremipsum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoremipsumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoremipsumApplication.class, args);
+	}
+}

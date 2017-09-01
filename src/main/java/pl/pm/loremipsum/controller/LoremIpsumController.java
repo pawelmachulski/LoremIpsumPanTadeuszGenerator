301a8bf2149b8ec4377af2df6a36d266e01e09fa
Pawel Machulski <pawel.machulski94@gmail.com>
2017-09-01 16:37:35 +0200

    added generate option - generating loremipsum after clicking GENERATE

--- src/main/java/pl/pm/loremipsum/controller/LoremIpsumController.java
+++ src/main/java/pl/pm/loremipsum/controller/LoremIpsumController.java
@@ -2,13 +2,21 @@ package pl.pm.loremipsum.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoremIpsumController {
 
     @GetMapping("/")
     public String home(){
+        return "home";
+    }
+
+    @PostMapping("/")
+    public String generate(ModelMap modelMap){
+        modelMap.addAttribute("generator","Lorem ipsum dolor sit amet magna. Integer mi non enim aliquam imperdiet. Nullam vulputate tellus non orci. Integer semper facilisis felis, dignissim nibh. Aliquam adipiscing elit. Aliquam eget elit arcu, eget augue. Praesent scelerisque urna arcu quis dolor. Vestibulum ornare interdum, lacus. Vivamus ornare, odio eget wisi vel nonummy sagittis. Vestibulum vel bibendum iaculis malesuada id, neque. Vestibulum enim. Aliquam erat eu pede ultrices posuere cubilia Curae, Sed sed justo. Phasellus tempor enim diam magna auctor varius, felis in neque quis massa metus gravida vel, adipiscing diam eu libero. Duis quis wisi. Phasellus vulputate eu, fringilla mi, eu dui eget pede dictum a, mauris. Etiam ullamcorper, risus suscipit.");
         return "home";
     }
 }

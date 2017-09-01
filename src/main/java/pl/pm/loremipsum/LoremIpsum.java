2d43d8a1f60fb03f586c04d16c5b46eec6fd67ea
Pawel Machulski <pawel.machulski94@gmail.com>
2017-09-01 16:08:42 +0200

    added app to heroku

--- src/main/java/pl/pm/loremipsum/LoremIpsum.java
+++ src/main/java/pl/pm/loremipsum/LoremIpsum.java
@@ -1,4 +1,9 @@
 package pl.pm.loremipsum;
 
 public interface LoremIpsum {
+
+    String generateParagraphs(int number);
+    String generateSentences(int number);
+    String generateWords(int number);
+
 }
--- src/main/java/pl/pm/loremipsum/controller/LoremIpsumController.java
+++ src/main/java/pl/pm/loremipsum/controller/LoremIpsumController.java
@@ -1,4 +1,14 @@
 package pl.pm.loremipsum.controller;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class LoremIpsumController {
+
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
 }
